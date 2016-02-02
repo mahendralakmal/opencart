8525f1b863b222b0c1cebcc89265d421b640d346
mahendra lakmal <mahendralakmal@gmail.com>
2016-02-02 12:19:41 +0530

    initial setup and header modified

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/opencart/admin/');
+define('HTTP_CATALOG', 'http://localhost:8888/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/opencart/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8888/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Mahen/Projects/opencart/upload/admin/');
+define('DIR_SYSTEM', '/Users/Mahen/Projects/opencart/upload/system/');
+define('DIR_LANGUAGE', '/Users/Mahen/Projects/opencart/upload/admin/language/');
+define('DIR_TEMPLATE', '/Users/Mahen/Projects/opencart/upload/admin/view/template/');
+define('DIR_CONFIG', '/Users/Mahen/Projects/opencart/upload/system/config/');
+define('DIR_IMAGE', '/Users/Mahen/Projects/opencart/upload/image/');
+define('DIR_CACHE', '/Users/Mahen/Projects/opencart/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/Mahen/Projects/opencart/upload/system/storage/download/');
+define('DIR_LOGS', '/Users/Mahen/Projects/opencart/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/Mahen/Projects/opencart/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/Mahen/Projects/opencart/upload/system/storage/upload/');
+define('DIR_CATALOG', '/Users/Mahen/Projects/opencart/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '525625');
+define('DB_DATABASE', 'justpartsinc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Mahen/Projects/opencart/upload/catalog/');
+define('DIR_SYSTEM', '/Users/Mahen/Projects/opencart/upload/system/');
+define('DIR_LANGUAGE', '/Users/Mahen/Projects/opencart/upload/catalog/language/');
+define('DIR_TEMPLATE', '/Users/Mahen/Projects/opencart/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/Mahen/Projects/opencart/upload/system/config/');
+define('DIR_IMAGE', '/Users/Mahen/Projects/opencart/upload/image/');
+define('DIR_CACHE', '/Users/Mahen/Projects/opencart/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/Mahen/Projects/opencart/upload/system/storage/download/');
+define('DIR_LOGS', '/Users/Mahen/Projects/opencart/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/Mahen/Projects/opencart/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/Mahen/Projects/opencart/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '525625');
+define('DB_DATABASE', 'justpartsinc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
